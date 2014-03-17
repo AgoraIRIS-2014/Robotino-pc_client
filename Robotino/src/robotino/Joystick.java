b62f6e0056aad2ab0f244d8c591d1762d01fa51a
Hellscream <Hellscream@Hellscream-PC>
2014-03-17 16:58:05 +0100

    Ebauche

--- Robotino/src/robotino/Joystick.java
+++ Robotino/src/robotino/Joystick.java
@@ -1,5 +1,39 @@
 package robotino;
 
-public class Joystick {
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
 
-}
+public class Joystick extends JFrame {
+	private Pannel pan = new Pannel();
+	public Joystick() {                
+	    this.setTitle("RobotinoJoystick");
+	    this.setSize(600, 600);
+	    this.setLocationRelativeTo(null);      
+	    this.setResizable(false);
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    this.setContentPane(pan);
+	    this.setVisible(true);
+	    
+	    go();
+	}
+  
+	private void go(){
+		for(int i = -50; i < pan.getWidth(); i++)
+		{
+		int x = pan.getXCenter(), y = pan.getYCenter();
+		x++;
+		y++;
+		pan.setXCenter(x);
+		pan.setYCenter(y);
+		pan.repaint();
+		try {
+		Thread.sleep(10);
+		} catch (InterruptedException e) {
+		e.printStackTrace();
+		}
+		}
+	}
+}      
+	
+
